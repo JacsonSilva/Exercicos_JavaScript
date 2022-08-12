162a92c87a7ec427c2a220d796a2881ed2852d7a
Jacson <jacson.analistaderede@gmail.com>
2022-08-12 10:07:36 -0400

    resolvendo exercicios da lista JS

---
+++ js-intermediario/exer01.js
@@ -0,0 +1,8 @@
+/* Criando uma Variavél hojeVaiChover*/
+
+var hojeVaiChover = true;
+if (hojeVaiChover){
+    console.log("Leve o seu Guarda chuva que hoje cairá um temporal")
+} else{
+    console.log("Fique tranquilo que hoje não vai chover")
+}
---
+++ js-intermediario/exer02.js
@@ -0,0 +1,35 @@
+/*criando varias Variaveis de condicionais*/
+/* Exerciao A*/
+var num1 = 5;
+var num2 = 1;
+
+
+if(num1 > num2){
+    console.log("O numero", num1 ,"é maior que ", num2)
+}else{
+console.log("não é maior")
+
+}
+
+/* Exerciao B*/
+
+var num1 = 2000000;
+var num2 = 3000000000;
+
+
+if(num1 < num2){
+    console.log("O numero", num1 ,"é menor que ", num2)
+}else{
+console.log("false")
+
+}
+
+/* Exerciao C*/
+
+var banana = true;
+
+if(banana == banana){
+    console.log("Banana é igual a Banana")
+}else{
+    console.log("False")
+}
